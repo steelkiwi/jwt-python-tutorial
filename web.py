09ffd2f6477ba6008bc726048f70e61311dc7600
Alexey Milogradov <alexey.milogradov@gmail.com>
2016-02-07 23:57:39 +0200

    Auth middleware

--- web.py
+++ web.py
@@ -35,5 +35,25 @@ async def login(request):
     jwt_token = jwt.encode(payload, JWT_SECRET, JWT_ALGORITHM)
     return json_response({'token': jwt_token.decode('utf-8')})
 
-app = web.Application()
+async def get_user(request):
+    return json_response({'user': str(request.user)})
+
+async def auth_middleware(app, handler):
+    async def middleware(request):
+        request.user = None
+        jwt_token = request.headers.get('authorization', None)
+        if jwt_token:
+            try:
+                payload = jwt.decode(jwt_token, JWT_SECRET,
+                                     algorithms=[JWT_ALGORITHM])
+            except (jwt.DecodeError, jwt.ExpiredSignatureError):
+                return json_response({'message': 'Token is invalid'},
+                                     status=400)
+
+            request.user = User.objects.get(id=payload['user_id'])
+        return await handler(request)
+    return middleware
+
+app = web.Application(middlewares=[auth_middleware])
+app.router.add_route('GET', '/get-user', get_user)
 app.router.add_route('POST', '/login', login)
